bbefd66c2a7d37b2da5a1ed140f9d713113f84cf
Carolina Nonnenmacher Stange <carolina.stange@narwalsistemas.com.br>
2025-07-04 14:14:19 -0300

    Subindo testes automatizados

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
-
-
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  viewportWidth: 375,
-  viewportHeight: 667,
+  projectId: "d66a2q",
+  viewportHeight: 880,
+  viewportWidth: 1280,
   e2e: {},
 })
+
+/* Forato mobile
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportWidth: 410,
+  viewportHeight: 860,
+  e2e: {}},
+}) */
